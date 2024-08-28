c1c65f165362d25fd3cab5c0a7c24149c4893126
aaditya-712 <aadityagdeshmukh9975@gmail.com>
2024-08-28 22:14:00 +0530

    modify

---
+++ DSA/6.3_join_touple.py
@@ -0,0 +1,6 @@
+thisIsTuple = ("red", "green", "blue", "voilet", "indigo", "yellow")
+number = (1, 2, 3, 4)
+
+tuple3 = thisIsTuple + number
+print(tuple3)
+print(len(tuple3))
--- Lab Reports/1_GCD_of_two_numbers.py
+++ Lab Reports/1_GCD_of_two_numbers.py
@@ -7,3 +7,4 @@ print("GCD of two numbers a and b is:", math.gcd(a, b))
 print("GCD of two numbers 48 and -12 is:", math.gcd(48, -12))
 print("GCD of two numbers -24 and -18 is:", math.gcd(-24, -18))
 print("GCD of two numbers -60 and 48 is:", math.gcd(-60, 48))
+
